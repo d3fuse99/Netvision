3c6ab5f5e5797f60914446c285f13cb76744a8b8
d3fuse <d3fuse99@gmail.com>
2026-08-27 18:15:18 +0300

    Add files via upload

---
+++ const.py
@@ -0,0 +1,20 @@
+VENDORS = {
+    "84:D8:1B": "TP-Link",
+    "00:0C:29": "VMware",
+    "BC:D1:D3": "Apple",
+    "40:8D:5C": "Samsung",
+    "D8:07:B6": "Samsung",
+    "9A:09:89": "Mobile",
+    "B6:AA:47": "Generic",
+    "D4:3A:2E": "Intel",
+    "F4:F5:DB": "Espressif",
+    "78:11:DC": "Xiaomi",
+    "00:1A:2B": "Huawei",
+    "50:C7:BF": "TP-Link",
+    "E4:5F:01": "Raspberry Pi",
+    "B8:27:EB": "Raspberry Pi",
+    "DC:A6:32": "Raspberry Pi",
+    "00:50:56": "VMware",
+    "08:00:27": "VirtualBox"
+}
+PORT = 5005
