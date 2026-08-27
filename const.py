cb069046c1ebd68ec8d34cd0a5f6c0ac407934bd
d3fuse <d3fuse99@gmail.com>
2026-08-27 18:14:25 +0300

    Delete const.py

--- const.py
+++
@@ -1,6 +0,0 @@
-VENDORS = {
-    "84:D8:1B": "TP-Link", "00:0C:29": "VMware", "BC:D1:D3": "Apple",
-    "40:8D:5C": "Samsung", "D8:07:B6": "Samsung", "9A:09:89": "Mobile",
-    "B6:AA:47": "Generic", "D4:3A:2E": "Intel"
-}
-PORT = 5005
